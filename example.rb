eebd2e9781c0d904e0adee1cf837ac21ac668e5b
Pedro Alcocer <pealco@gmail.com>
2008-08-17 16:10:04 -0400

    Created an example script.

---
+++ example.rb
@@ -0,0 +1,26 @@
+require("process_linger_data.rb")
+
+# A new Experiment object takes two arguments:
+#   (1) an Array that specifies the names of the experiment(s)
+#   (2) a Hash that specifies how the conditions should be expanded
+
+experiment_names = ["principB", "principC"]
+
+# Here, for example, the condition coded as TNN in the raw data is
+# expanded out to the factors RC, N, and U. You can specify as many
+# conditions and factors as necessary.
+
+factors = { "TNN" => %w(RC N U),
+            "TNM" => %w(RC M U),
+            "QNN" => %w(HN N U),
+            "QMN" => %w(HN M G)}
+
+experiment = Experiment.new(experiment_names, factors)
+
+# All processing is done when the object is created. There are two 
+# options for outputting the processed data. You can either output
+# it to STD_OUT (to check that everything went smoothly), or you can
+# save it to a file.
+
+puts experiment                # to STD_OUT
+experiment.to_file("data.txt") # to a file called data.txt
