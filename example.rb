ca4ddc8e3d0916fe3d436d1d18875884a55aa256
Pedro Alcocer <pealco@gmail.com>
2008-08-21 13:47:58 -0400

    Included comments about the new features.

--- example.rb
+++ example.rb
@@ -1,9 +1,12 @@
-#require("profile")
 require("linger-process.rb")
 
-# A new Experiment object takes two arguments:
-#   (1) an Array that specifies the names of the experiment(s)
-#   (2) a Hash that specifies how the conditions should be expanded
+# A new Experiment object takes four arguments:
+#   (1) required, Array that specifies the names of the experiment(s)
+#   (2) required, Hash that specifies how the conditions should be expanded
+#   (3) optional, String that specifies the directory containing the Linger output data, defaults to "../data/"
+#   (4) optional, Array of symbols that specifies what columns should be output, defaults to
+#           [:words, :word_lengths, :word_positions, :regions, :experiment, :subject, :item, :condition,
+#            :sentence_number, :list_position, :reading_times, :log_reading_times, :accuracy]
 
 experiment_names = ["principB", "principC"]
 
@@ -16,17 +19,18 @@ factors = { "TNN" => %w(RC N U),
             "QNN" => %w(HN N U),
             "QMN" => %w(HN M G)}
 
+directory = "./data/"
+
 columns = [:subject, :item, :condition, :factors, :regions, 
            :reading_times, :log_reading_times, :words, :word_lengths, 
            :word_positions, :experiment, :list_position, :accuracy]
 
-experiment = Experiment.new(experiment_names, factors, "data/", columns)
+experiment = Experiment.new(experiment_names, factors, directory, columns)
 
 # All processing is done when the object is created. There are two 
 # options for outputting the processed data. You can either output
 # it to STD_OUT (to check that everything went smoothly), or you can
 # save it to a file.
 
-#p experiment
-#puts experiment                # to STD_OUT
+puts experiment                # to STD_OUT
 experiment.to_file("data.txt") # to a file called data.txt
